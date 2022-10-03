83e0ea1757d87b30b66e36484f61bc67187c6dca
emmettgalles <eegalles@gmail.com>
2022-10-03 00:22:48 -0400

    changed to scale probability to most extreme roll occurence, avoid zero/100% probability cases

--- main.py
+++ main.py
@@ -6,7 +6,9 @@ Created on Sun Jul 24 12:44:28 2022
 """
 ## === IMPORTING MODULES ===
 import copy, random
-from operator import add
+import numpy as np
+import matplotlib.pyplot as plt
+from IPython import get_ipython
 
 ## === INITIATING VARIABLES ===
 # Create the dice (just a list with possible rolls)
@@ -21,43 +23,53 @@ actual_dist = copy.copy(target_dist)
 dynamic_prob = copy.copy(target_dist)
 # Track the rolls
 rolls = []
+# We need to preallocate the adaptive probability
+adaptive_prob = []
 
-## === FIRST ROLL ===
-# Roll the first dice, or in other words, choose a random number from dice with the probability given by fixed_prob
-first_roll = (random.choices(dice, weights=fixed_prob, k=1))
-# We now increment this in our actual_dist list
-actual_dist[first_roll[0]-2] += 1
-# Increment the target_dist
-target_dist = list(map(add, target_dist, fixed_prob))
-# Add the roll to the list of rolls
-rolls += first_roll
-
-## === SUBSEQUENT ROLLS ===
-
-# *** can initiate some type of iteration here ***
-for i in range(35):
-    
-    # Increment the target_dist
-    target_dist = list(map(add, target_dist, fixed_prob))
-    # We adjust the dynamic probability based on what the target_dist is. Note that before we roll, we adjust target_dist because that's what we'll be aiming for when we adjust dynamic_prob. To do that, we subtract actual_dist from target_dist, make any negative values 0, then normalize to whatever the sum of the nonzero values are, and that's the new dynamic_prob
-    #
-    # Subtract actual_dist from target_dist
-    diff_dist = list(map(add, target_dist, [-x for x in actual_dist]))
-    # Find negative numbers and set to 0
-    #
-    # Set counter
-    counter = 0
-    # Find the negative numbers
-    for dist in diff_dist:
-        if dist < 0:
-            diff_dist[counter] = 0
-        counter += 1
-    # Now we normalize
-    dynamic_prob[:] = [x / sum(diff_dist) for x in diff_dist]
-    # And then we can roll
-    roll = (random.choices(dice, weights=dynamic_prob, k=1))
-    # We now increment this in our actual_dist list
+## === ROLLING THE DICE ===
+# *** iteration can be done here ***
+# Set number of roles here (which will be the number we put in range)
+for i in range(72):
+    if i == 0:  # First roll
+        # Roll the dice, or in other words, choose a random number from dice with the probability given by fixed_prob
+        roll = (random.choices(dice, weights=fixed_prob, k=1))
+    else:
+        # Roll the dice with the adaptive_prob
+        roll = (random.choices(dice, weights=adaptive_prob, k=1))
+    # We now increment this in our actual_dist list. The subtracion of the roll's value by two is manipulating the value to match up with the correct index (ex: if a six is rolled, six is the 5th item in the list, and since Python indexes starting from zero, the correct index would be 4 = 6 - 2)
     actual_dist[roll[0]-2] += 1
-    # Add it to the list of rolls
+    # Add the roll to the list of rolls
     rolls += roll
-
+    # We need to be a little crafty here. What we're looking to do is figure out what the limiting dice value is. In other words, we want to see how which dice value has had the most improbable occurences, as that is what we'll base our adaptive probability off of. We will find that value, then scale our fixed_prob distribution so that the probability will correspond with how the distribution should look assuming that the limiting dice value can be rolled one more time. For example, imagine that we roll a six, four, and three elevens. Obviously the three elevens is a little lopsided, so what we'll do is scale the fixed_prob distribution so that the probability of rolling an eleven is equal to four (for four rolls). To do this, we'll scale the entire fixed_prob list by 4 / [2 / 36]. Once we have all those numbers after scaling, we'll have to account for the fact of what the past rolls have been by subtracting off those rolls from the distribution. Let's start writing stuff and I'll explain along the way.
+    #
+    # We need to find the limiting value we do this by piecewise dividing actual_dist by fixed_prob and seeing which index returns the greatest number. In the event of a tie, we need to randomly select which number is chosen.
+    scaled_dist = np.divide(actual_dist, fixed_prob)
+    # We now find the index (or indices) of the limiting value(s). If we have more than one, we will select one at random.
+    limiting_value_index = random.choice([i for i, x in enumerate(scaled_dist) if x == max(scaled_dist)])
+    # We now scale fixed_prob by the largest number of scaled_dist. When we do this, we create the statistically accurate distribution that would result in the limiting value's number of rolls occuring. Once this is done, then we need to add another distribution, this other distribution being a scaled version of fixed_prob so that our limiting value is equal to one. This allows the probability of the next roll to have a nonzero chance of rolling the limiting value.
+    #
+    # First scale fixed_prob by the largest number of scaled_dist
+    scaled_prob = [scaled_dist[limiting_value_index] * a for a in fixed_prob]
+    # Now create the additional roll distribution to add on
+    additional_dist = [1 / fixed_prob[limiting_value_index] * b for b in fixed_prob]
+    # Now sum them together
+    scaled_prob_additional = np.add(scaled_prob, additional_dist)
+    # We now subtract off previous rolls
+    adaptive_scaled_prob_additional = np.subtract(scaled_prob_additional, actual_dist)
+    # Now we normalize everything so it's nice to read
+    adaptive_prob = [c / sum(adaptive_scaled_prob_additional) for c in adaptive_scaled_prob_additional]
+    
+    ## === PLOTTING ===
+    # Let's try to visualize what's going on. We're gonna plot the fixed probability as a bar chart, then next to it we'll have the actual distribution, then next to THAT we'll have the adaptive probability
+    #
+    # Pop out the plot
+    get_ipython().run_line_magic('matplotlib', 'qt')
+    # Plot the fixed probability
+    plt.subplot(1, 3, 1)
+    plt.bar(dice, fixed_prob)
+    # Now the actual distribution
+    plt.subplot(1, 3, 2)
+    plt.bar(dice, [d / sum(actual_dist) for d in actual_dist])
+    # Now the adaptive probability
+    plt.subplot(1, 3, 3)
+    plt.bar(dice, adaptive_prob)
